acd9ae5f7b374ee8a434b0b7c784c7b246667299
MarvinH <marvin.hinzmann@gmail.com>
2018-10-23 20:27:46 +0200

    gulp workflow post css

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
 var gulp = require("gulp"),
     watch = require("gulp-watch"),
     postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssvars = require("postcss-simple-vars"),
-    nested = require("postcss-nested"),
-    cssImport = require ("postcss-import");
+    autoprefixer = require("autoprefixer");
+    
+    gulp.task("default", function() {
+        console.log("Default Task");
+    });
 
-gulp.task("default", function(){
-    console.log("default task");
-});     
+    gulp.task("html", function() {
+        console.log("jadada");
+    });
 
-gulp.task("styles", function(){
-    console.log("done");
-    return gulp.src("C:\Users\Marvin\Desktop\ez\money\css\style.css")
-    .pipe(gulp.dest("C:\Users\Marvin\Desktop"));
-});
+    gulp.task("css", function() {
+       return gulp.src("./money/css/style.css")
+        .pipe(postcss([autoprefixer]))
+        .pipe(gulp.dest("./money/temp/styles"));
+    });
 
-gulp.task("watch", function(){
-    watch("./money/css/style.css", function(){
-        console.log("hallo");
-        gulp.start("styles");
+    gulp.task("watch", function(){
+
+        watch("./money/html/index.html",function(){
+            gulp.start("html");
+        });
+        watch("./money/css/**/*.css",function(){
+            gulp.start("css");
+        });
 
     });
-});
